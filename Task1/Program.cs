d39a6c6e3f4b71c7a9e26841108c5282d87ac8e2
DaKu <do.holyada@gmail.com>
2022-06-18 13:48:22 +0300

    T1.2

---
+++ Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите цикл, который принимает на вход
+// два числа (A и B) и возводит число A в натуральную
+// степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+// *VAR!*
+// Console.WriteLine("Insert number A: ");
+// int A = int.Parse(Console.ReadLine());
+// Console.WriteLine("Insert number B: ");
+// int B =int.Parse(Console.ReadLine());
+//  Console.WriteLine(Math.Pow(A,B));
+
+//Var2
+void PrintPower(int A, int B)
+{
+    int result = 1;
+    for (int i = 0; i < B; i++)
+    {
+        result = result * A;
+    }
+    Console.WriteLine(result);
+}
+Console.WriteLine("Insert number A: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Insert number B: ");
+int B =int.Parse(Console.ReadLine());
+
+PrintPower(A,B);
+ Console.WriteLine("Is  " + A + " to the " + B + " power ");
