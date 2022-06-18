97e47bd565ea21637d85ef35928f1526335b85fd
DaKu <do.holyada@gmail.com>
2022-06-18 14:18:42 +0300

    T2.2

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на
+// вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int NumberSum(int A)
+{
+    int sum = 0;
+    while ( A != 0)
+    {
+        sum += A % 10;
+        A /= 10;
+    }
+    return sum;
+}
+
+Console.WriteLine("Insert number: ");
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Sum of digits in the number "+A+" is "+NumberSum(A));
